bff95991daa54bce7e78a8ea6e9f8021e3d88a1e
Shahriar Hasan <flaidexdev@gmail.com>
2023-10-25 02:12:03 +0600

    Update TodoDashboard.tsx

--- src/components/TodoDashboard.tsx
+++ src/components/TodoDashboard.tsx
@@ -35,7 +35,7 @@ const TodoDashboard: React.FC = () => {
         todoData.filter((todo) => {
           return status === "Total"
             ? category === "All"
-              ? todoData
+              ? todo
               : todo.category.toLowerCase() === category.toLowerCase()
             : category === "All"
             ? status === "Completed"
